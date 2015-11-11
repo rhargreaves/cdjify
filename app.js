fcad5cacaea1234d5e2b8530125f966f27cb4845
rhargreaves <kiyouta@gmail.com>
2015-11-11 22:42:13 +0000

    POC

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 
+var Converter = require("csvtojson").Converter;
+var fs = require("fs");
+var converter = new Converter({});
+
 var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-    res.render('home');
+
+	converter.on("end_parsed", function(jsonArray) {
+		res.render('home', jsonArray);
+	});
+
+	fs.createReadStream("./test.csv").pipe(converter);
 });
 
 app.listen(3000);
