b9351af2b01297c838880ee16fcc708e896817d1
rhargreaves <robert.hargreaves@7digital.com>
2015-11-13 17:09:41 +0000

    Fix hanging response; add title

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+'use strict';
 var express = require('express');
 var exphbs  = require('express-handlebars');
 
 var Converter = require("csvtojson").Converter;
 var fs = require("fs");
-var converter = new Converter({});
 
 var app = express();
 
@@ -12,15 +12,22 @@ app.set('view engine', 'handlebars');
 app.use(express.static('assets'));
 app.get('/', function (req, res) {
 
+	var converter = new Converter({});
 	var fileName = req.query.file;
+	var title = req.query.title;
+	var data = {};
 
 	converter.on("end_parsed", function(jsonArray) {
-
-		console.log(jsonArray);
-		res.render('home', jsonArray);
+		data = { tracks: jsonArray, title: title };
+		console.log(data);
 	});
 
-	fs.createReadStream(fileName).pipe(converter);
+	var stream = fs.createReadStream(fileName);
+	stream.pipe(converter);
+	stream.on('close', function() {
+		console.log("close");
+		res.render('home', data);
+	});
 });
 
 app.listen(3000);
